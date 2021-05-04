ee5bb43cb7ad944456c8aac0c8169af7026ff578
Flora Davies <daviesfgo@gmail.com>
2021-05-04 14:33:25 +0100

    added deposit function which updates balance only so far, next will store amount date and balance in an object I think

--- spec/bankAccount.test.js
+++ spec/bankAccount.test.js
@@ -1,8 +1,17 @@
 const Account = require('../src/bankAccount')
 
 describe('creates a new account', () => {
+  const newAccount = new Account();
   test('it initialises with 0 balance', () => {
-    const newAccount = new Account();
+   
     expect(newAccount.balance).toBe(0);
+  });
+});
+
+describe('deposit', () => {
+   const newAccount = new Account();
+  test('it deposits an amount to the card', () => {
+    newAccount.deposit(15);
+    expect(newAccount.balance).toBe(15);
   });
 });
