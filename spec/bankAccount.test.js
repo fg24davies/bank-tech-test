a5903428410cc32d9925d9426e7f11fd4fb92b8b
Flora Davies <daviesfgo@gmail.com>
2021-05-05 11:49:51 +0100

    not passing the formatting printing statement test but haven't committed in a while. Loop needs to go from end of array to start and I need to make the money have 2 decimal places

--- spec/bankAccount.test.js
+++ spec/bankAccount.test.js
@@ -12,17 +12,40 @@ describe('creates a new account', () => {
 
 describe('deposit', () => {
    const newAccount = new Account();
-  test('it deposits an amount to the account', () => {
-    newAccount.deposit(15);
-    expect(newAccount.balance).toEqual(15);
+  test('it creates a credit transaction', () => {
+    const date = '01/01/01';
+    newAccount.deposit(15, date);
+    expect(newAccount.statement).toEqual([{'amount': 15, 'date': '01/01/01', 'type': 'credit', 'balance': newAccount.balance}]);
   });
 });
 
 describe('withdraw', () => {
    const newAccount = new Account();
-  test('it withdraws an amount from the account', () => {
+  test('it creates a new debit transaction', () => {
     newAccount.deposit(30);
-    newAccount.withdraw(15);
-    expect(newAccount.balance).toEqual(15);
+    const date = '01/01/01';
+    newAccount.withdraw(10, date);
+    expect(newAccount.statement[1]).toEqual({'amount': 10, 'date': '01/01/01', 'type': 'debit', 'balance': newAccount.balance});
   });
+});
+
+describe('print statement', () => {
+  const newAccount = new Account();
+  test('it creates the statement header', () => {
+      expect(newAccount.statementHeader()).toEqual("date || credit || debit || balance");
+  });
+   const date1 = '10/01/2012'
+   const date2 = '13/01/2012'
+   const date3 = '14/01/2012'
+   newAccount.deposit(1000, date1)
+   newAccount.deposit(2000, date2)
+   newAccount.withdraw(500, date3)
+  test('it prints the list of transactions', () => {
+      expect(newAccount.printStatement()).toEqual( `date || credit || debit || balance \n
+      ${date3} || || 500.00 || 2500.00 \n
+      ${date2} || 2000.00 || || 3000.00 \n
+      ${date1} || 1000.00 || || 1000.00`);
+  });
+
+
 });
--- src/bankAccount.js
+++ src/bankAccount.js
@@ -5,17 +5,51 @@ class Account {
     this.statement = [];
   }
 
-  deposit(amount) {
+  deposit(amount, date = new Date().toLocaleDateString) {
     this.balance += amount;
+    const transaction = { 
+      'amount': amount,
+      'date': date,
+      'type': 'credit',
+      'balance': this.balance
+   };
+   this.statement.push(transaction);
   }
 
-  withdraw(amount) {
+  withdraw(amount, date = new Date().toLocaleDateString) {
     this.balance -= amount;
+    const transaction = { 
+      'amount': amount,
+      'date': date,
+      'type': 'debit',
+      'balance': this.balance
+    };
+    this.statement.push(transaction);
+  }
+
+  statementHeader() {
+    return "date || credit || debit || balance\n";
+  }
+
+  printStatement() {
+    console.log('the statement length', this.statement.length)
+    let transactionFormat = '';
+    for (let i = 0; i < this.statement.length; i++) {
+      transactionFormat += `${this.statement[i]['date']} || `;
+      if (this.statement[i]['type'] === 'debit') {
+        transactionFormat += `|| ${this.statement[i]['amount']} || ${this.statement[i]['balance']}\n`;
+      } else {
+        transactionFormat += `${this.statement[i]['amount']} || || ${this.statement[i]['balance']}\n`;
+      }
+    }
+    console.log(this.statementHeader() + transactionFormat);
+    return statementHeader() + transactionFormat;
   }
 
 
-
-
+  // _formatTransaction(date, type, balance) {
+    
+  // }
 
 }
 
